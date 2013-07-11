7791238180c32bc71bb3aa9e5976c0db24dc31a0
daniel anggrianto <d.anggrianto@gmail.com>
2013-07-11 23:08:29 +0000

    grader recieve URL and write to file

--- grader.js
+++ grader.js
@@ -20,10 +20,10 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -54,6 +54,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+var checkURL = function(result, checkfile){
+	$ = cheerio.load(result);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+	        out[checks[ii]] = present;
+	}
+	return out;
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +76,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var fileHTML = program.file;
+    if(program.url){
+	    fileHTML="web.html";
+	    var checkJson = restler.get(program.url).on('complete', function(result){
+		    //saving as html
+		    fs.writeFile(fileHTML, result);
+		    //return checkURL(result, program.checks);
+	    });
+	    console.log(checkJson);
+    }
+    //var checkJson = checkHtmlFile(fileHTML, program.checks);
+//    var outJson = JSON.stringify(checkJson, null, 4);
+  //  console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
